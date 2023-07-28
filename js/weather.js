a7c5b36dbd5c15641760601d5ebe0f650db49779
zhouwei35 <zhouweiqaq@163.com>
2023-07-28 13:47:27 +0800

    Site updated: 2023-07-28 13:47:26

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "c102b73ea67648d299dd525024bed676"
+    }
+  }
